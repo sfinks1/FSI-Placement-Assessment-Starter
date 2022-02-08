bed930ee5a95a9ec463a4b19e7102d03e0383d05
Samantha Finks <sfinks1@users.noreply.github.com>
2022-02-07 18:25:58 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,114 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Samantha Finks" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+// We'll use these variables to track the counts of each cookie type.
+let gb = 0      //Gingerbread
+let cc = 0      //Chocolate Chip
+let ss = 0      //Sugar Sprinkle
+//Total Quantity.
+var gbQuantity;
+var ccQuantity;
+var ssQuantity;
+var total = 0;
+var totalQuantity = document.getElementById("qty-total");
+var gbQuantity = document.getElementById("qty-gb");
+var ccQuantity = document.getElementById("qty-cc");
+var ssQuantity = document.getElementById("qty-sugar");
 
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
+//Updating display name
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+document.getElementById("credit").textContent = `Created by ${yourName}`
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// Event listener for clicks on the "+" button for Gingerbread cookies.
+
+document.getElementById("add-gb").addEventListener('click', function() {
+    
+    {
+        gb++
+    }
+   
+    gbQuantity.textContent = gb;
+    
+    total = total + 1;
+    totalQuantity.textContent = total;
+    console.log("Gingerbread + was clicked!")
+})
+// Event listener to decrease quantity on the "-" button for Gingerbread cookies.
+
+document.getElementById("minus-gb").addEventListener("click", function() {
+if(gb > 0)
+    {
+        gb-- 
+    }
+    
+    gbQuantity.textContent = gb;
+
+    total = total - 1;
+    totalQuantity.textContent = total;
+    
+    console.log("Gingerbread - was clicked!")
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+// Event listener for clicks on the "+" button for Chocolate Chip cookies
+document.getElementById("add-cc").addEventListener("click", function() {
+    
+    {
+        cc++
+    }
+    
+    ccQuantity.textContent = cc;
+    
+    total = total + 1;
+    totalQuantity.textContent = total;
+    
+    console.log("Chocolate Chip + was clicked!")
+})
+// Event listener to decrease quantity on the "-" button for Chocolate Chip cookies.
+
+document.getElementById("minus-cc").addEventListener("click", function() 
+{
+    if(cc > 0)
+    {
+        cc--
+    }
+    
+    ccQuantity.textContent = cc;
+    total = total - 1;
+    totalQuantity.textContent = total;
+    
+    
+   
+    
+    console.log("Chocolate Chip - was clicked!")
+})
+// Event listener for clicks on the "+" button for Sugar Sprinkle cookies
+document.getElementById("add-sugar").addEventListener("click", function() {
+    
+    {
+        ss++
+    }
+    
+    ssQuantity.textContent = ss;
+    totalQuantity = document.getElementById("qty-total");
+    total = total + 1;
+    totalQuantity.textContent = total;
+    console.log("Sugar Sprinkle + was clicked!")
+})
+// Event listener to decrease quantity on the "-" button for Sugar Sprinkle cookies.
+document.getElementById("minus-sugar").addEventListener("click", function() {
+    if ( ss > 0)
+    {
+        ss--
+    }    
+    
+    ssQuantity.textContent = ss;
+    
+    total = total - 1;
+    
+    totalQuantity.textContent = total;
+    console.log("Sugar Sprinkle - was clicked!")
+})
